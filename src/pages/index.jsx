09238d37d7f29318a5c2f7a0e717938f131d1558
Alexandros Ntitoras <a.ntitoras@gmail.com>
2021-02-10 09:52:50 +0200

    set timeout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,6 @@ import { Link, navigate } from 'gatsby';
 export default function Home() {
   setTimeout(() => {
     navigate('/app/login');
-  }, 100);
-  return <Link to="/app/login">Login</Link>;
+  }, 20);
+  return <Link to="/app/login" />;
 }
